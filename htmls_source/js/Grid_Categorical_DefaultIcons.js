94eb2ad27766df1dda664b0c25ad14b56ae3bf87
Long Pham <long.pham@Ipsos.com>
2020-09-30 15:16:48 +0700

    The Grid_Categorical_Scales is needed to adjust after the categories are grouped together

--- htmls_source/js/Grid_Categorical_DefaultIcons.js
+++ htmls_source/js/Grid_Categorical_DefaultIcons.js
@@ -4,22 +4,24 @@ $(document).ready(function(){
     console.log("hi")
     switch($('.mrQuestionTable').prop('tagName').toLowerCase()){
         case 'table':
+            
             //$(".mrQuestionTable").addClass('grid-container');
 
             $(".mrQuestionTable tbody tr").get().map(function(row){
 
+                /*
                 $(row).addClass('grid-row');
                 $(row).before($(row).find('td:first'));
                 $(row).prev().wrap("<tr class='grid-row'/>");
                 $(row).prev().find('td:first').removeAttr('style');
                 $(row).prev().find('td:first').attr('colspan', $(row).find('td').length);
-
+                */
                 $(row).find('td').get().map(function(cell){
-
+                    console.log($(cell).attr('class'));
                     $(cell).removeAttr('style');
                 });
             });
-            
+            /*
             $(".mrQuestionTable tbody tr:first").remove();
 
             var $header_scales = $(".mrQuestionTable tbody tr:first");
@@ -33,6 +35,7 @@ $(document).ready(function(){
                     $(row).after("<tr class='grid-row'>" + $header_scales.html() + "</tr>");
                 }
             });
+            */
             break;
     }
 });
--- htmls_source/js/Grid_Categorical_Scales.js
+++ htmls_source/js/Grid_Categorical_Scales.js
@@ -6,43 +6,105 @@ $(document).ready(function(){
         case 'table':
             $(".mrQuestionTable").addClass('grid-container');
 
+            console.log("hi");
+
+            var idx_row = 0;
+            var numberofcells = 0;
+            var $header_scales = undefined;
+
             $(".mrQuestionTable tbody tr").get().map(function(row){
                 
+                var idx_cell = 0;
+                
                 $(row).addClass('grid-row');
-                $(row).before($(row).find('td:first'));
-                $(row).prev().wrap("<tr class='grid-row'/>");
-                $(row).prev().find('td:first').removeAttr('style');
-                $(row).prev().find('td:first').attr('colspan', $(row).find('td').length);
                 
                 $(row).find('td').get().map(function(cell){
+                    
+                    if($(cell).find('input[type=radio]').length == 0){
+                        
+                        if((idx_row == 0 && $(cell).find('.mrQuestionText').length == 0) || (idx_row != 0 && $(cell).find('.mrQuestionText').length == 1)){
+                            
+                            $(row).before($(cell));
+                            
+                            if($(row).prev().attr('rowspan') != undefined){
+                                $(row).prev().removeAttr('rowspan');
+                                $(row).prev().wrap("<tr class='grid-subgroup'/>");
+                            } else{
+                                if($(row).prev().find('.mrQuestionText').length == 1){
+                                    $(row).prev().wrap("<tr class='grid-attr'/>");
 
-                    if($(cell).find('.mrQuestionText').length == 0){
-                        $(cell).addClass('grid-scale');
+                                    var text = $(row).prev().find('.mrQuestionText').html();
+                                    var regExp = new RegExp("<img.*?>");
+                                    
+                                    if(regExp.test(text)){
+                                        var s1 = text.replace(regExp, "");
+                                        var s2 = text.replace(s1, "");
+
+                                        s1 = s1.replace(/<.*?(\/>)/, "");
+                                        s1 = s1.replace(/<.*?(>)/, "");
+                                        
+                                        $(row).prev().find('.mrQuestionText').html(s1 + "<br/>" + s2);
+                                    }
+                                } else {
+                                    $(row).prev().wrap("<tr class='grid-row'/>");
+                                }
+                            }
+                            
+                            $(row).prev().find('td:first').removeAttr('style');
+
+                            if(idx_row == 0) numberofcells = $(row).find('td').length;
+
+                            $(row).prev().find('td:first').attr('colspan', numberofcells);
+                        }
                     } else {
+                        
                         $(cell).addClass('grid-cell');
+
+                        if($(cell).find('input[type=radio]').length == 1){
+                            
+                            $(cell).parent().removeClass('grid-row');
+                            $(cell).parent().addClass('grid-scale');
+
+                            $item = $(cell).find('input[type=radio]');
+                            $item.wrap("<span class='cat-default'/>");
+                            $item.after("<label for='" + $item.prop('id') + "'></label>");
+                        }
                     }
-
+                    
                     $(cell).removeAttr('style');
-
-                    $item = $(cell).find('input[type=radio]');
-                    $item.wrap("<span class='cat-default'/>");
-                    $item.after("<label for='" + $item.prop('id') + "'></label>");
+                    idx_cell++;
                 });
+
+                if(idx_row == 0){
+                    $(".mrQuestionTable tbody tr:first").remove();
+
+                    $header_scales = $(".mrQuestionTable tbody tr:first");
+                    $header_scales.find('td').addClass('grid-cell');
+                    $(".mrQuestionTable tbody tr:first").remove();
+                } else {
+                    if($(row).attr('class') == 'grid-scale'){
+                        $(row).before("<tr class='grid-label'>" + $header_scales.html() + "</tr>");
+                    
+                        if(!$(row).is('tr:last')){
+                            $(row).after("<tr><td style='padding:3px' colspan='" + $(row).find('.grid-scale').length + "'></td></tr>")
+                        }
+                    }
+                }
+
+                idx_row++;
             });
             
-            $(".mrQuestionTable tbody tr:first").remove();
-
-            //$(".mrQuestionTable tbody").before($(".mrQuestionTable tbody tr:first"));
-            //$(".mrQuestionTable tbody").prev().wrap("<thead/>")
             
-            var $header_scales = $(".mrQuestionTable tbody tr:first");
-            $(".mrQuestionTable tbody tr:first").remove();
             
+            //var $header_scales = $(".mrQuestionTable tbody tr:first");
+            //$(".mrQuestionTable tbody tr:first").remove();
+            
+            /*
             $(".mrQuestionTable tbody tr").get().map(function(row){
                 
                 if($(row).find('.mrGridCategoryText').length == 1){
-                    $(row).removeClass('grid-row');
-                    $(row).addClass('grid-attr');
+                    //$(row).removeClass('grid-row');
+                    //$(row).addClass('grid-attr');
                     
                     var text = $(row).find('.mrQuestionText').html();
                     var regExp = new RegExp("<img.*?>");
@@ -57,13 +119,14 @@ $(document).ready(function(){
                         $(row).find('.mrQuestionText').html(s1 + "<br/>" + s2);
                     }
                     
-                    $(row).after("<tr class='grid-row'>" + $header_scales.html() + "</tr>");
+                    $(row).after(/"<tr class='grid-row'>" + $header_scales.html() + "</tr>");
                 } else if ($(row).find('.grid-scale').length > 0){
                     if(!$(row).is('tr:last')){
                         $(row).after("<tr><td style='padding:3px' colspan='" + $(row).find('.grid-scale').length + "'></td></tr>")
                     }
                 }
             });
+            */
             break;
     }
 });
